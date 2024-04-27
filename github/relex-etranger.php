17f5c4b5e02df7ed72550eccc7d6ad75566e48b8
Belkacemi Amine <aminenahd4636@gmail.com>
2024-04-27 16:30:20 +0100

    add traitement etranger et relex etranger

---
+++ github/relex-etranger.php
@@ -0,0 +1,75 @@
+<link rel="stylesheet" href="stylemisejr.css">
+<link rel="stylesheet" href="relex-formation.css">
+    <div id="tableEtranger-relex" class="tables-container">
+
+
+        <div class="table-header">
+            <p>Traitement des demandes de etranger</p>
+        </div>
+
+
+        
+        <!-- add form to get num demande cliquee -->
+        
+            <div class="header_fixed" id="table-demande-etranger-relex">
+            <table id="table-etranger-relex">
+                <thead>
+                    <tr>
+                        <th class="th-relex-etranger">N° demande</th>
+                        <th class="th-relex-etranger">Date demande</th>
+                        <th class="th-relex-etranger">Pays</th>
+                        <th class="th-relex-etranger">Nom demandeur</th>
+                        <th class="th-relex-etranger">Objet</th>
+                    </tr>
+                </thead>
+                <tbody>
+                <?php
+                    
+                    $sql="Select * from `etranger`";
+                    $result=mysqli_query($conn,$sql);
+                    if ($result) {
+                        
+                        while ($row=mysqli_fetch_assoc($result)) {
+                            
+                            
+
+                            $NumDemande = $row['NumDemande'];
+                            $DateDemande = $row['DateDemande'];
+                            $Pays = $row['Pays'];
+                            $Demandeur = $row['Demandeur'];
+                            $ObjetMission = $row['ObjetMission'];
+                            
+                            echo '<tr class="tr-relex-etranger">
+                            <td class="tr-relex-etranger">'.$NumDemande.'</td>
+                            <td class="tr-relex-etranger">'.$DateDemande.'</td>
+                            <td class="tr-relex-etranger">'.$Pays.'</td>
+                            <td class="tr-relex-etranger">'.$Demandeur.'</td>
+                            <td class="tr-relex-etranger">'.$ObjetMission.'</td>
+                        </tr>';
+                        }
+                    }
+                
+                ?>
+                </tbody>
+            </table>
+           
+            </div>
+        
+        
+            <a href="relex.php">
+        <button id="retour-table-demande">Retour</button>
+        </a>
+
+
+    </div>
+
+<!--*******************                    ************************ -->
+<!--****************   div traitement etranger   ***************** -->
+<!--*******************                    ************************ -->
+
+<?php require_once 'traitement-etranger.php'; ?>
+
+<!--*******************                    ************************ -->
+<!--*************** FIN div traitement etranger  ***************** -->
+<!--*******************                    ************************ -->
+<script src="relex-etranger.js"></script>
--- github/relex.js
+++ github/relex.js
@@ -9,6 +9,9 @@ tableFormationRelex.style.display = 'none';
 let tableCourseRelex = document.getElementById('tableCourse-relex');
 tableCourseRelex.style.display = 'none';
 
+let tableEtrangerRelex = document.getElementById('tableEtranger-relex');
+tableEtrangerRelex.style.display = 'none';
+
 
 let tableMission = document.getElementById('table-mission');
 
@@ -51,8 +54,13 @@ trCourse1.addEventListener("click",function(){
     tableCourseRelex.style.display = 'block';
 })
 
+// click in tr course il m'affich table des demandes
 
-
+let trEtranger1 = document.getElementById("tr-etranger")
+trEtranger1.addEventListener("click",function(){
+    tableMission.style.display = 'none';
+    tableEtrangerRelex.style.display = 'block';
+})
 
 
 
@@ -63,6 +71,13 @@ let home1 = document.getElementById("home")
 home1.addEventListener('click',function () {
     let tableFormationRelex = document.getElementById("tableFormation-relex")
     tableFormationRelex.style.display = 'none';
+
+    let tableCourseRelex = document.getElementById("tableCourse-relex")
+    tableCourseRelex.style.display = 'none';
+
+    let tableEtrangerRelex = document.getElementById("tableEtranger-relex")
+    tableEtrangerRelex.style.display = 'none';
+
     let Notification = document.getElementById("notification-relex");
     Notification.style.display='block'
 })
