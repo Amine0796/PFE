77e8aa8e2b08523c27955ff7db43b63a9772b3b9
eki35 <ikr900545@gmail.com>
2024-04-27 16:31:24 +0100

    css course

--- github/traitement-course.php
+++ github/traitement-course.php
@@ -20,171 +20,74 @@
                 <p class="header-text">Traiter une demande de formation</p>
                     <div class="container-relex">
                         <div class="mini-container">
-                            <label for="direction-formation-relex">
-                                Direction
-                                <input type="text" name="direction-formation-relex" id="direction-formation-relex" value="<?php echo $_SESSION["demandeur"]?>" >
-                            </label><!-- <br><br> -->
-                            
-                            <label for="departement-formation-relex">
-                                Département
-                                <input type="text" name="departement-formation-relex" readonly>
-                            </label><!-- <br><br> -->
+                        <label for="direction-course">
+                Direction
+                <input type="text" name="direction-course" readonly>
+            </label><!-- <br><br> -->
+            
+            <label for="departement-course">
+                Département
+                <input type="text" name="departement-course" readonly>
+            </label><!-- <br><br> -->
 
-                            <label for="demande-dffuctuee-par-formation-relex">
-                                Demande effectuée par
-                                <input type="text" name="demande-dffuctuee-par-formation-relex" readonly>
-                            </label><!-- <br><br> -->
+            <label for="demandeur-course">
+                Demande effectuée par
+                <input type="text" name="demandeur-course" value="<?php echo $_SESSION["username"]; ?>" readonly>
+            </label><!-- <br><br> -->
 
-                            <label for="compte-analytique-formation-relex">
-                                Compte analytique
-                                <input type="text" name="compte-analytique-formation-relex" readonly>
-                            </label><!-- <br><br> -->
+            <label>
+                Num Demande 
+                <input type="number" name="num-demande-course" value="1" min="1" required>
+            </label>
 
-                            <label for="num-demande-formation-relex">
-                                Num Demande 
-                                <input type="number" name="num-demande-formation-relex" value="1" min="1" required>
-                            </label>
+            <label for="compte-analytique-course">
+                Compte analytique
+                <input type="text" name="compte-analytique-course" readonly>
+            </label><!-- <br><br> -->
 
-                            <label for="date-demande-formation-relex">
-                                Date de demande
-                                <input type="date" name="date-demande-formation-relex" value="<?php echo date('Y-m-d'); ?>" readonly>
-                            </label><!-- <br><br> -->
+            <label for="date-demande-course">
+                Date de demande
+                <input type="date" name="date-demande-course" value="<?php echo date('Y-m-d'); ?>" readonly>
+            </label><!-- <br><br> -->
 
-                            <label for="destinataire-formation-relex">
-                                Destinataire
-                                <input type="text" name="destinataire-formation-relex" value="RELEX" readonly>
-                            </label>
+            <label for="destinataire-course">
+                Destinataire
+                <input type="text" name="destinataire-course" value="RELEX" readonly>
+            </label>
 
-                            <label for="du-formation-relex">
-                                Date début 
-                                <input type="date" name="du-formation-relex" value="<?php echo date('Y-m-d'); ?>" readonly>
-                            </label>
+            <label for="destination-course">
+                Destination
+                <input type="text" name="destination-course">
+            </label>
 
-                            
-                            <label for="au-formation-relex">
-                                Date fin
-                                <input type="date" name="au-formation-relex" value="<?php echo date('Y-m-d'); ?>" readonly>
-                            </label><!-- <br><br> -->
-
-                            <label for="region-deroulement-formation-relex">
-                                Région deroulement formation
-                                <select name="region-deroulement-formation-relex"  >
-                                    <option value="nord">NORD</option>
-                                    <option value="sud">SUD</option>
-                                    <option value="est">EST</option>
-                                    <option value="ouest">OUEST</option>
-                                </select><!-- <br><br> -->
-                            </label>
-                            
-                    
-                            <label for="lieu-formation-relex">
-                                lieu de la formation 
-                                <input type="text" name="lieu-formation-relex" readonly >
-                            </label>
-                            
-                    
-                            <label for="itineraire-formation-relex">
-                                Itineraire
-                                <input type="text" name="itineraire-formation-relex" readonly>
-                            </label>
-                    
-                            <label for="objet-formation-relex">
-                                Objet de la formation
-                                <input type="text" name="objet-formation-relex" readonly>
-                            </label>
-                            
-                            <label for="demande-hebergement-formation-relex">
-                                Demande hébergement
-                                <select name="demande-hebergement-formation-relex"  readonly>
-                                    <option >Oui</option> 
-                                    <option >Non</option>
-                                </select><!-- <br><br> -->
-                            </label>
-
-                            <label for="demande-billet-formation-relex">
-                                Demande billetterie
-                                <select name="demande-billet-formation-relex"  readonly>
-                                    <option >Oui</option> 
-                                    <option >Non</option>
-                                </select><!-- <br><br> -->
-                            </label>
-
-                            <label for="noms-missionaires-formation-relex">
-                                Nom(s) du(des) missionnaire(s)
-                                <input type="text" name="noms-missionaires-formation-relex" readonly>
-                            </label>
-                            
-
-                            <label for="moyen-de-transport-aller-formation-relex">
-                                Moyen de Transport aller 
-                                <select name="moyen-transport-aller-formation-relex" >
-                                    <option value="avion">Avion</option>
-                                    <option value="vehicule">Vehicule</option>
-                                </select><!-- <br><br> -->
-                            </label>
-                            
-
-                            <label for="moyen-de-transport-retour-formation-relex">
-                                Moyen de transport retour 
-                                <select name="moyen-transport-retour-formation-relex">
-                                    <option value="avion">Avion</option>
-                                    <option value="vehicule">Vehicule</option>
-                                </select><!-- <br><br> -->
-                            </label>
-                        <!-- </div> -->
-                            
-
-                        <fieldset>
-                        <legend>Avion</legend>
-                        <div class="avion">
-                                <label for="demande-vehicule-vers-aeroport-formation-relex">
-                                    Demande Vehicule service vers l'aéroport 
-                                    <select name="demande-vehicule-aeroport-formation-relex">
-                                        <!-- <option ></option> -->
-                                        <option value="oui">Oui</option> 
-                                        <option value="non">Non</option>
-                                    </select><!-- <br><br> -->
-                                </label>
-                            
-                
-                                <label for="point-de-depart-avion-formation-relex">
-                                    Point du départ
-                                    <input type="text" name="point-depart-avion-formation-relex" readonly>
-                                </label>
-                        </div>
-                        </fieldset>
-                        
-                        <fieldset>
-                            <legend>Véhicule</legend>
-                            <div class="vehicule">
-                                <label for="demande-vehicule-vers-formation-relex">
-                                    Demande Vehicule vers lieu de formation 
-                                    <select name="demande-vehicule-formation-relex">
-                                        <!-- <option ></option> -->
-                                        <option >Oui</option> 
-                                        <option >Non</option>
-                                    </select><!-- <br><br> -->
-                                </label>
-                            
-                                <label for="point-de-depart-vehicule-formation-relex">
-                                    Point du départ
-                                    <input type="text" name="point-depart-vehicule-formation-relex" readonly>
-                                </label>
-                            </div>
-                        </fieldset>
-
-                        <label for="organisme-formateur-relex">
-                            Organisme formateur
-                            <input type="text" name="organisme-formateur-relex" readonly>
-                        </label>
+            <label for="objet-demande-course">
+                Objet de la demande
+                <input type="text" name="objet-demande-course">
+            </label><!-- <br><br> -->
 
 
-                        <label for="observation-formation-relex">
-                            Observations
-                            <input type="text" name="observation-formation-relex" readonly>
-                        </label>
+            <label for="date-depart-course">
+                Date du départ
+                <input type="date" name="date-depart-course" value="<?php echo date('Y-m-d'); ?>">
+            </label><!-- <br><br> -->
 
-                        <input type="hidden"  name="etat-formation" readonly value="NON TRAITE" hidden>
+            <label for="heure-depart-course">
+                Heure du départ
+                <input type="text" name="heure-depart-course">
+            </label><!-- <br><br> -->
+
+            <label for="point-depart-course">
+                Point du départ
+                <input type="text" name="point-depart-course">
+            </label>
+            
+            <label for="missionnaires-course">
+                Nom(s) du(des) missionnaire(s)
+                <input type="text" name="missionnaires-course">
+            </label>
+
+
+            <input type="hidden"  name="etat-course" readonly value="NON TRAITE" hidden>
 
                         </div>
 
